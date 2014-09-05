8393ad90c68617d34a1b063c9146807990220a24
phereford <phereford@gmail.com>
2014-09-05 06:21:25 -0400

    delay hatchbuck

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
             end
 
             @user.save
-            Hatchbuck.new(@user).post_data
+            Hatchbuck.new(@user).delay.post_data
         end
 
         # Send them over refer action
